786d07f412f4e97f99eafb7261ca1ee367374f9d
tsteelematc <tsteele@madisoncollege.edu>
2026-04-04 12:08:57 -0500

    Placeholders for things...

--- src/PizzaToppings.tsx
+++ src/PizzaToppings.tsx
@@ -1,8 +1,36 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 const PizzaToppings = () => { 
   return (
-    <h2>
-      Choose your pizza toppings...
-    </h2>
+
+    <Card>
+        <Card.Body>
+            <Card.Title
+                className="mb-3"
+            >
+                Choose your pizza toppings...
+            </Card.Title>
+            <Button
+                variant="outline-primary"
+            >
+                All
+            </Button>
+            <Button
+                className="ms-2"
+                variant="outline-primary"
+            >
+                None
+            </Button>
+            <div>
+                Pizza toppings go here...
+            </div>
+            <h3>
+                Total: $0.00
+            </h3>
+        </Card.Body>
+
+    </Card>
   );
 };
 
